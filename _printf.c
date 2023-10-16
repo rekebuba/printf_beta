be07b786d4cc241cdd1c999f958665ac4757cc58
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-10-16 14:47:28 +0300

    my commit

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+  *_printf - displays to the stdout according to a format
+  *@format: format string congaing the characters and specifiers
+  *Return: length of the formatted output string.
+  */
+
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list args;
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(args, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(args);
+	return (count);
+}
